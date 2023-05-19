ea1bdc102cac59853048aa0bab4d5c09a592d415
Gabriel-Moura06 <123772567+Gabriel-Moura06@users.noreply.github.com>
2023-05-19 11:50:13 -0300

    primeira aula de python

---
+++ biel.py
@@ -0,0 +1,28 @@
+print("mensagem que eu quero", end=".")
+
+print('fulano é meu "amigo"')
+
+print(1+1)
+
+print("2 x 2 =" ,2*2)
+
+print("10³ = ", 10**3)
+
+print("10³ =", pow(10,3))
+
+nome = ""
+print(nome)
+nome = 1
+print(nome)
+nome = 1+1
+print(nome)
+nome = True
+
+D1 = 5
+D2 = "5"
+print(D1 + int(D2))
+
+D1 = 5.1
+D2 = "5"
+print(int(D1 + int(D2)))
+print(round(D1 + int(D2)))
